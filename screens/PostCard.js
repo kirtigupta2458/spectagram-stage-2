b1b70bfa61af8664740cb397ea2029e687147b2b
kirtigupta2458 <72331388+kirtigupta2458@users.noreply.github.com>
2022-03-11 19:40:56 +0530

    Update PostCard.js

--- screens/PostCard.js
+++ screens/PostCard.js
@@ -22,20 +22,18 @@ export default class PostCard extends Component {
                 <View style={styles.cardContainer}>
                     <View style={styles.authorContainer}>
                         <View style={styles.authorImageContainer}>
-                            <Image
-                                source={require("../assets/profile_img.png")}
-                                style={styles.profileImage}
-                            ></Image>
+                            
+                            
                         </View>
                         <View style={styles.authorNameContainer}>
-                            <Text style={styles.authorNameText}>{this.props.post.author}</Text>
+                           
                         </View>
                     </View>
                     <Image source={require("../assets/post.jpeg")} style={styles.postImage} />
                     <View style={styles.captionContainer}>
-                        <Text style={styles.captionText}>
-                            {this.props.post.caption}
-                        </Text>
+                        
+                        
+                        
                     </View>
                     <View style={styles.actionContainer}>
                         <View style={styles.likeButton}>
